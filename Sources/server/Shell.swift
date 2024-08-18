761f60447077d186f494a8df32a1bea0dea0f08c
myaccount20232828 <146480147+myaccount20232828@users.noreply.github.com>
2024-08-17 22:04:10 -0400

    Update Shell.swift

--- Sources/server/Shell.swift
+++ Sources/server/Shell.swift
@@ -1,43 +1,6 @@
 import Foundation
 import Darwin
 
-//var JBRoot = FindJBRoot()!
-var JBRoot = FileManager.JBRoot
-
-extension FileManager {
-    static var JBRoot = FindJBRoot()!
-}
-
-func FindJBRoot() -> String? {
-    if FileManager.default.fileExists(atPath: "/usr/bin/bash") {
-        //Rootfull JB
-        print("Rootfull JB Detected")
-        return ""
-    } else if FileManager.default.fileExists(atPath: "/var/jb/usr/bin/bash") {
-        //Rootless JB
-        print("Rootless JB Detected")
-        return "/var/jb"
-    } else {
-        //RootHide JB
-        do {
-            let ApplicationsPath = "/var/containers/Bundle/Application"
-            if let JBRoot = try FileManager.default.contentsOfDirectory(atPath: ApplicationsPath).filter({$0.hasPrefix(".jbroot")}).first {
-                print("RootHide JB Detected")
-                return "\(ApplicationsPath)/\(JBRoot)"
-            } else {
-                return nil
-            }
-        } catch {
-            print(error)
-            return nil
-        }
-    }
-}
-
-@discardableResult func shell(_ command: String, _ Root: Bool = false) -> Int {
-    return runCommand("/usr/bin/bash", ["-c", command], Root ? 0 : 501, JBRoot)
-}
-
 // Define C functions
 @_silgen_name("posix_spawnattr_set_persona_np")
 @discardableResult func posix_spawnattr_set_persona_np(_ attr: UnsafeMutablePointer<posix_spawnattr_t?>, _ persona_id: uid_t, _ flags: UInt32) -> Int32
